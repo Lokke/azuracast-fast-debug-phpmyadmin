70758133d9e6cda02eaf6e987423f26fced4627a
Lokke <accounts.lokke@googlemail.com>
2024-02-17 15:27:32 +0100

    Update setup-phpmyadmin.sh

--- setup-phpmyadmin.sh
+++ setup-phpmyadmin.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Setze den Pfad zur azuracast.env-Datei
+# Set the path to the azuracast.env file
 AZURACAST_ENV_PATH="/var/azuracast/azuracast.env"
 
-# Überprüfe, ob die azuracast.env-Datei existiert
+# Check if the azuracast.env file exists
 if [ ! -f "$AZURACAST_ENV_PATH" ]; then
-    echo "Die Datei azuracast.env wurde nicht gefunden unter $AZURACAST_ENV_PATH"
+    echo "The file azuracast.env was not found at $AZURACAST_ENV_PATH"
     exit 1
 fi
 
-# Lies die Umgebungsvariablen aus der azuracast.env-Datei
+# Read the environment variables from the azuracast.env file
 MYSQL_USER=$(grep ^MYSQL_USER= "$AZURACAST_ENV_PATH" | cut -d= -f2)
 MYSQL_PASSWORD=$(grep ^MYSQL_PASSWORD= "$AZURACAST_ENV_PATH" | cut -d= -f2)
 
-# Erstelle das Docker Compose-File
+# Prompt for the port to be published
+read -p "Enter the port to publish for phpMyAdmin (default: 8080): " PORT
+PORT=${PORT:-8080}
+
+# Create the Docker Compose file
 cat <<EOF > docker-compose.yml
 version: '3.1'
 
@@ -22,7 +26,7 @@ services:
     image: phpmyadmin/phpmyadmin
     restart: always
     ports:
-      - "8855:80"
+      - "$PORT:80"
     environment:
       - PMA_HOST=azuracast
       - PMA_USER=${MYSQL_USER}
@@ -35,15 +39,17 @@ networks:
     external: true
 EOF
 
-echo "Das Docker Compose-File wurde erfolgreich erstellt."
+echo "The Docker Compose file was created successfully."
 
-# Abfrage, ob das erstellte Skript gestartet werden soll
-read -p "Möchten Sie das erstellte Skript starten? (ja/nein): " start_response
+# Prompt to start the containers
+read -p "Do you want to start the containers? ([yes]/no): " start_response
+start_response=${start_response:-yes}
 
-if [ "$start_response" == "ja" ]; then
-    # Abfrage, ob es als Daemon laufen soll
-    read -p "Soll das Skript als Daemon laufen? (ja/nein): " daemon_response
-    if [ "$daemon_response" == "ja" ]; then
+if [ "$start_response" = "yes" ] || [ "$start_response" = "y" ]; then
+    # Prompt whether containers should run as daemons
+    read -p "Should the containers run as daemons? ([Yes]/no): " daemon_response
+    daemon_response=${daemon_response:-Yes}
+    if [ "$daemon_response" = "Yes" ] || [ "$daemon_response" = "yes" ] || [ "$daemon_response" = "Y" ]; then
         docker-compose up -d
     else
         docker-compose up
